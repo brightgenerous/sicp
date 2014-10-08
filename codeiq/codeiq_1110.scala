2c2d65ac533cf3c64f6c4563f19f3bca3062deae
brightgenerous <katou.akihiro@gmail.com>
2014-10-08 23:04:58 +0900

    2014.10.08

--- codeiq/codeiq_1110.scala
+++ codeiq/codeiq_1110.scala
@@ -4,40 +4,67 @@
 
 // scala 2.11
 
+// answer
+//   6055
+
 object Main {
 
   def main(args: Array[String]) = {
+    val t = System.currentTimeMillis
+
     println(patterns(8).size)
+
+    // 1350 ms
+    println((System.currentTimeMillis - t) + " ms")
   }
 
-  def patterns(n: Int) = {
-    val revs = reverses(n)
-    (0 until n).permutations
-      .filter { pattern => ok(revs, pattern) }
-  }
+  def patterns(n: Int) =
+    (1 to n).permutations
+      .filter { pattern => println(""); ok(n, pattern) }
 
-  def ok(revs: Seq[Seq[Int]], pattern: Seq[Int]) = {
-    val sands = pattern.map { p => (p, p) }
-    val fall = sands.map { sand => (sand._1, sand._2 - 1) }
-    val rs = revs.head
-    val rev = fall.map {
-        case (p, s) if rs.contains(p) =>
-          (p, p)
-        case ps => ps
+  // (capacity: Int, current: Int)
+  type SandWatch = (Int, Int)
+
+  // (index: Int, SandWatch)
+  type Step = (Int, Seq[SandWatch])
+
+  def ok(n: Int, pattern: Seq[Int]) = {
+    def itr(count: Int, step: Seq[SandWatch],
+            steps: Seq[Step]): Boolean = {
+
+      // finish?
+      if (step.forall { case (_, s) => s == 1 }) {
+        return true;
       }
-    true
+
+      // fall
+      val fall = step.map { case (t, s) => (t, Math.max(s - 1, 0)) }
+
+      // reverse
+      val indexes = reverseIndexes(n, count % n, pattern(count % n))
+      val rev = fall.zipWithIndex.map {
+          case ((t, s), i) if indexes.contains(i) =>
+            (t, t - s)
+          case ((t, s), i) => (t, s)
+        }
+      val last = (count % n, rev)
+
+      // contains?
+      if (steps.contains(last)) {
+        return false;
+      }
+
+      //println(last)
+
+      // go to next
+      itr(count + 1, rev, last +: steps)
+    }
+
+    // start!!
+    itr(0, pattern.map { i => (i, i) }, Seq())
   }
 
-  def reverses(n: Int) =
-    Seq(
-      Seq(0),
-      Seq(1, 2),
-      Seq(2, 3, 4),
-      Seq(3, 4, 5, 6),
-      Seq(4, 5, 6, 7, 0),
-      Seq(5, 6, 7, 0, 1, 2),
-      Seq(6, 7, 0, 1, 2, 3, 4),
-      Seq(7, 0, 1, 2, 3, 4, 5, 6)
-    )
+  def reverseIndexes(n: Int, start: Int, sand: Int) =
+    (start until (start + sand)).map(_ % n)
 }
 
