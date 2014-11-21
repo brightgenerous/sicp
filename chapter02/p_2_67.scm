92da6992b745d56923115d00de84920b205be10c
brightgenerous <katou.akihiro@gmail.com>
2014-11-22 01:38:27 +0900

    20141122

--- chapter02/p_2_67.scm
+++ chapter02/p_2_67.scm
@@ -150,7 +150,7 @@
 (define (encode-symbol symbol tree)
   (define (itr tree)
     (if (leaf? tree)
-      (if (eq? (symbol-leaf tree) symbol)
+      (if (equal? (symbol-leaf tree) symbol)
         '()
         #f)
       (let
@@ -206,10 +206,111 @@
       (cddr leaf-set)))
   (define (itr leaf-set)
     (if (null? (cdr leaf-set))
-      leaf-set
+      (car leaf-set)
       (itr (merge-heads leaf-set))))
   (itr leaf-set))
 
 ;(print (make-leaf-set '((D 1) (B 2) (A 4) (C 1))))
 (print (successive-merge (make-leaf-set '((D 1) (B 2) (A 4) (C 1)))))
+
+
+(print "----------------")
+
+(print "問題2.70")
+(print "--")
+
+(define tree-1950 (generate-huffman-tree '((A 2) (NA 16) (BOOM 1) (SHA 3) (GET 2) (YIP 9) (JOB 2) (WAH 1))))
+(define song '(GET A JOB SHA NA NA NA NA NA NA NA NA GET A JOB SHA NA NA NA NA NA NA NA NA WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP SHA BOOM))
+(define encoded (encode song tree-1950))
+
+(display "tree => ")
+(print tree-1950)
+(display "encoded => ")
+(print encoded)
+(display "size => ")
+(print (length encoded))
+
+(display "8 = 2^3 => ")
+(print (* (length song) 3))
+
+
+(print "----------------")
+
+(print "問題2.71")
+(print "--")
+
+;
+; tree-n5
+;
+;                      (left right (A B C D E) 31)
+;                       /    \
+;                      /   ('leaf E 16)
+;                     /
+;                (left right (A B C D) 15)
+;                 /    \
+;                /   ('leaf D 8)
+;               /
+;          (left right (A B C) 7)
+;           /    \
+;          /   (`leaf C 4)
+;         /
+;    (left right (A B) 3)
+;     /    \
+;    /   (`leaf B 2)
+;   /
+; (`leaf A 1)
+;
+;
+; tree-n10
+;
+;                      (left right (A B C D E F G H I J) 1023)
+;                       /    \
+;                      /   ('leaf J 512)
+;                     /
+;                (left right (A B C D E F G H I) 511)
+;                 /    \
+;                /   ('leaf I 256)
+;               /
+;          (left right (A B C D E F G H) 255)
+;           /    \
+;          /   (`leaf H 128)
+;         /
+;    (left right (A B C D E F G) 127)
+;     /    \
+;    /   (`leaf G 64)
+;   /
+; (left right (A B C D E F) 63)
+;  |    \
+;  |  ('leaf F 32)
+;  |
+;  tree-n5
+;
+
+(define tree-n5 (generate-huffman-tree '((A 1) (B 2) (C 4) (D 8) (E 16))))
+(define tree-n10 (generate-huffman-tree '((A 1) (B 2) (C 4) (D 8) (E 16) (F 32) (G 64) (H 128) (I 256) (J 512))))
+
+(display "tree-n5 => ")
+(print tree-n5)
+(display "tree-n10 => ")
+(print tree-n10)
+
+(display "(length (encode '(E) tree-n5)) => ")
+(print (length (encode '(E) tree-n5)))
+; => 1
+(display "(encode '(A) tree-n5) => ")
+(print (length (encode '(A) tree-n5)))
+; => 5 - 1
+
+(display "(length (encode '(J) tree-n10)) => ")
+(print (length (encode '(J) tree-n10)))
+; => 1
+(display "(length (encode '(A) tree-n10)) => ")
+(print (length (encode '(A) tree-n10)))
+; => 10 - 1
+
+
+(print "----------------")
+
+(print "問題2.72")
+(print "--")
 
