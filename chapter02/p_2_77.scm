86e7f2275bccc48b90dce39fd23afd3dfbf9ac0f
brightgenerous <katou.akihiro@gmail.com>
2014-12-25 22:18:46 +0900

    20141225

--- chapter02/p_2_77.scm
+++ chapter02/p_2_77.scm
@@ -254,6 +254,7 @@
 
 ;; -----
 
+(print "--")
 (print "問題2.78")
 
 (define (attach-tag type-tag contents)
@@ -283,11 +284,14 @@
    (print (apply-generic 'add s-num1 s-num2))
    (display "(apply-generic 'add num1 num2) => ")
    (print (apply-generic 'add num1 num2))
+   (display "(add s-num1 num2) => ")
+   (print (add s-num1 num2))
 ))
 
 
 ;; -----
 
+(print "--")
 (print "問題2.79")
 
 ((lambda()
@@ -316,8 +320,12 @@
 ((lambda()
    (define s1 (make-scheme-number 2))
    (define s2 (make-scheme-number 2))
+   (define s21 (make-scheme-number 2))
+   (define s22 (make-scheme-number 22))
    (display "(apply-generic 'equ? s1 s2) => ")
    (print (apply-generic 'equ? s1 s2))
+   (display "(apply-generic 'equ? s21 s22) => ")
+   (print (apply-generic 'equ? s21 s22))
 ))
 
 ((lambda()
@@ -337,6 +345,7 @@
 
 ;; -----
 
+(print "--")
 (print "問題2.80")
 
 ((lambda()
@@ -353,7 +362,7 @@
 
 ((lambda()
   (put 'zero? '(complex)
-       (lambda (x) (and (= (magnitude x) 0) (= (angle x) 0))))
+       (lambda (x) (and (= (real-part x) 0) (= (imag-part x) 0))))
 ))
 
 ;; --
@@ -384,4 +393,189 @@
    (display "(apply-generic 'zero? c2) => ")
    (print (apply-generic 'zero? c2))
 ))
+
+
+;; -----
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (if (= (length args) 2)
+            (let ((type1 (car type-tags))
+                  (type2 (cadr type-tags))
+                  (a1 (car args))
+                  (a2 (cadr args)))
+            (let ((t1->t2 (get-coercion type1 type2))
+                  (t2->t1 (get-coercion type2 type1)))
+              (cond (t1->t2
+                      (apply-generic op (t1->t2 a1) a2))
+                    (t2->t1
+                      (apply-generic op a1 (t2->t1 a2)))
+                    (else
+                      (error "No method for these types"
+                             (list op type-tags))))))
+          (error "No method for these types"
+                 (list op type-tags)))))))
+
+(define (symbol-append . symbols)
+  (string->symbol (apply string-append (map symbol->string symbols))))
+
+(define (get-coercion type1 type2)
+  (get
+    (symbol-append type1 '-> type2)
+    (list type1 type2)))
+
+(define (put-coercion type1 type2 proc)
+  (put
+    (symbol-append type1 '-> type2)
+    (list type1 type2)
+    proc))
+
+
+(put-coercion 'scheme-number 'complex
+              (lambda (n) (make-complex-from-real-imag n 0)))
+
+;; -----
+
+(print "--")
+(print "問題2.81")
+
+(define (scheme-number->scheme-number n) n)
+(define (complex->complex z) z)
+(put-coercion 'scheme-number 'scheme-number
+              scheme-number->scheme-number)
+(put-coercion 'complex 'complex complex->complex)
+
+((lambda ()
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+  (put 'exp '(scheme-number scheme-number)
+     (lambda (x y) (tag (expt x y))))
+))
+(define (exp x y) (apply-generic 'exp x y))
+
+;; a
+
+(print (exp (make-scheme-number 3) (make-scheme-number 2)))
+;;(print (exp (make-complex-from-real-imag 1 1) (make-complex-from-real-imag 2 2)))
+;;  => (apply-generic 'exp ('complex (1 . 1)) ('complex (2 . 2)))
+;;    を繰り返す
+;;
+
+;; b
+;;
+;; 変換可能な異なる型が見つからなかった場合にループを抜ける必要がある
+;; どちらか一方をもう一方の型に合わせるので、変換後も同じ型ならばループする
+;;
+;;
+
+;; -----
+
+(print "--")
+(print "問題2.82")
+
+(define (apply-generic op . args)
+  ;; data : ('rational (1 . 1))
+  ;; tag  : 'complex
+  ;; =>   : ('complex (1 . 0))
+  (define (data-to-type data tag)
+    (let ((data-type (type-tag data)))
+      (if (eq? data-type tag)
+        data
+        (let
+          ((d->t (get-coercion data-type tag)))
+          (if d->t (d->t data) #f)
+        )
+      )
+    )
+  )
+  ;; datas : (('rational (1 . 1)) ('complex (2 . 0)))
+  ;; tag   : 'complex
+  ;; =>    : (('complex (1 . 0)) ('complex (2 . 0)))
+  (define (datas-to-type datas tag)
+    (define (inner datas)
+      (if (null? datas)
+        '()
+        (let
+          ((head (data-to-type (car datas) tag)))
+          (if head
+            (let
+              ((tail (inner (cdr datas))))
+              (if tail (cons head tail) #f)
+            )
+            #f
+          )
+        )
+      )
+    )
+    (inner datas)
+  )
+  ;; datas : (('rational (1 . 1)) ('complex (2 . 0)))
+  ;; tags  : ('rational 'complex)
+  ;; =>    : (('complex (1 . 0)) ('complex (2 . 0)))
+  (define (datas-to-any datas tags)
+    (define (inner tags)
+      (if (null? tags)
+        #f
+        (let
+          ((ds-t-ty (datas-to-type datas (car tags))))
+          (if ds-t-ty ds-t-ty (inner (cdr tags)))
+        )
+      )
+    )
+    (if (null? datas) '() (inner tags))
+  )
+
+  (let ((type-tags (map type-tag args)))
+    (let
+      ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (let
+          ((datas (datas-to-any args type-tags)))
+          (if datas
+              (apply apply-generic (cons op datas))
+              (error "No method for these types" (list op type-tags))
+          )
+        )
+      )
+    )
+  )
+)
+
+;; --
+(print (add (make-scheme-number 2) (make-scheme-number 3)))
+(print (add (make-complex-from-real-imag 2 2) (make-complex-from-real-imag 3 3)))
+(print (add (make-scheme-number 4) (make-complex-from-real-imag 5 5)))
+(print (sub (make-complex-from-real-imag 4 4) (make-scheme-number 5)))
+;; --
+;;(print (sub (make-rational 4 4) (make-scheme-number 5)))
+;; ERROR: No method for these types (sub (rational scheme-number))
+;;
+
+;; -----
+
+(print "--")
+(print "問題2.83")
+((lambda ()
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+
+  (put 'raise '(scheme-number)
+     (lambda (n) (make-complex-from-real-imag n 0)))
+
+  (put 'raise '(rational)
+     (lambda (n) (make-scheme-number (/ (numer n) (denom n)))))
+))
+(define (raise x) (apply-generic 'raise x))
+
+
+(display "(raise (make-scheme-number 3)) => ")
+(print (raise (make-scheme-number 3)))
+(display "(raise (make-rational 2 3)) => ")
+(print (raise (make-rational 2 3)))
+(display "(raise (apply-generic 'raise (make-rational 2 3))) => ")
+(print (raise (apply-generic 'raise (make-rational 2 3))))
 
